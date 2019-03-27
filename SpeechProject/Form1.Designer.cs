1807c4c6b13cc7c328ad8865d01d00d88e26f89d
DenisShulyak <denshufly@mail.ru>
2019-03-27 22:07:18 +0600

    project

---
+++ SpeechProject/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SpeechProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(143, 33);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(221, 211);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "-Log-";
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(244, 281);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(120, 23);
+            this.btnEnable.TabIndex = 1;
+            this.btnEnable.Text = "Enable Voice Control";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Enabled = false;
+            this.btnDisable.Location = new System.Drawing.Point(435, 281);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(158, 23);
+            this.btnDisable.TabIndex = 2;
+            this.btnDisable.Text = "Disable Voice Control";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Speech Recognition";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+    }
+}
+
---
+++ SpeechProject/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Synthesis;
+using System.Speech.Recognition;
+//using System.Speech.Recognition;
+//using System.Speech.Synthesis;
+
+namespace SpeechProject
+{
+    public partial class Form1 : Form
+    {
+
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         private void button1_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisable.Enabled = true;
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            Choices commands = new Choices();
+
+            // commands.Add("Я пишу сообщение");
+            //  commands.Add("точка");
+            // SpeechRecognizedEventArgs eventArgs = new SpeechRecognizedEventArgs();
+            //SpeechRecognizedEventArgs args = new SpeechRecognizedEventArgs();
+
+             GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(e.ToString());
+
+
+            Grammar grammar = new Grammar(gBuilder);
+            
+            recEngine.LoadGrammarAsync(new DictationGrammar());
+       
+            recEngine.SetInputToDefaultAudioDevice();
+         
+            recEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Speech_SpeechRecognized);
+           
+            
+        }
+
+         void Speech_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+            string str = e.Result.Text;
+            switch(e.Result.Text)
+            {
+                case "точка":
+                    richTextBox1.Text += ".";
+
+                    break;
+            }
+            if (e.Result.Text != "точка")
+            {
+                richTextBox1.Text += e.Result.Text + " ";
+            }
+            }
+
+        
+    }
+}
